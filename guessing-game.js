04baa902df2a067dfc47f27089cd5469a72f65a5
Santam Kumai <santtamk@gmail.com>
2024-10-05 22:57:46 +0530

    first commit

---
+++ guessing-game.js
@@ -0,0 +1,52 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let secretNumber = 12;
+
+const checkGuess = (num) => {
+  let num1 = Number(num);
+  if (num1 > secretNumber) {
+    console.log("Too high");
+    return false;
+  } else if (num1 < secretNumber) {
+    console.log("Too low");
+    return false;
+  } else if (num1 === secretNumber) {
+    console.log("Correct!");
+    return true;
+  }
+};
+
+const askGuess = (guess) => {
+  if (checkGuess(guess)) {
+    console.log("You win!");
+    rl.close();
+  } else {
+    rl.question("Enter a guess: ", askGuess);
+  }
+};
+
+//
+
+const randomInRange = (min, max) => {
+  const minCeiled = Math.ceil(min); //as it rounds up to a higher value
+  const maxFloored = Math.floor(max); //rounds down
+
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+};
+// secretNumber = randomInRange(min, max);
+
+// console.log(randomInRange(15, 30));
+
+const askRange = (max) => {
+  rl.question("Enter a min number: ", (min) => {
+    console.log(`I'm thinking of a number between ${min} and ${max}...`);
+    secretNumber = randomInRange(min, max);
+    rl.question("Enter a guess: ", askGuess);
+  });
+};
+rl.question("Enter a max number: ", askRange);
